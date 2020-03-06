086dcc45ba36ce431e7bc032e325b29ef55fc91c
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-03-06 14:20:36 +0100

    chore: upgrade core dependency (#5)
    contributors: @mmross

--- Tests/Functional/FunctionalTest.php
+++ Tests/Functional/FunctionalTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Auxmoney\OpentracingMonologBundle\Tests\Functional;
 
 use Auxmoney\OpentracingBundle\Tests\Functional\JaegerFunctionalTest;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 class FunctionalTest extends JaegerFunctionalTest
@@ -14,9 +13,9 @@ class FunctionalTest extends JaegerFunctionalTest
     {
         $this->setUpTestProject('default');
 
-        $p = new Process(['symfony', 'console', 'test:monolog'], 'build/testproject');
-        $p->mustRun();
-        $output = $p->getOutput();
+        $process = new Process(['symfony', 'console', 'test:monolog'], 'build/testproject');
+        $process->mustRun();
+        $output = $process->getOutput();
         $traceId = substr($output, 0, strpos($output, ':'));
         self::assertNotEmpty($traceId);
 
@@ -26,20 +25,15 @@ class FunctionalTest extends JaegerFunctionalTest
 
     protected function setUpTestProject(string $projectSetup): void
     {
-        $filesystem = new Filesystem();
-        $filesystem->mirror(sprintf('Tests/Functional/TestProjectFiles/%s/', $projectSetup), 'build/testproject/');
+        $this->copyTestProjectFiles($projectSetup);
 
-        $p = new Process(['composer', 'dump-autoload'], 'build/testproject');
-        $p->mustRun();
-        $p = new Process(['symfony', 'console', 'cache:clear'], 'build/testproject');
-        $p->mustRun();
+        $this->composerDumpAutoload();
+        $this->consoleCacheClear();
     }
 
     protected function tearDown()
     {
-        $p = new Process(['git', 'reset', '--hard', 'reset'], 'build/testproject');
-        $p->mustRun();
-        $p = new Process(['docker', 'stop', 'jaeger']);
-        $p->mustRun();
+        $this->gitResetTestProject();
+        $this->dockerStopJaeger();
     }
 }

a3d1a849b4ce1aac6384ad617396faae83005e5f
Christian A. Wolf <mail@cawolf.de>
2020-12-27 13:48:09 +0100

    fix: install dependencies

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -2,6 +2,7 @@
 shopt -s extglob
 
 cd build/testproject/
+composer require auxmoney/opentracing-bundle-monolog
 rm -fr vendor/auxmoney/opentracing-bundle-monolog/*
 cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-monolog
 composer dump-autoload

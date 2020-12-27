53c4572c570ca45393ac3e0ac830abf04c9ae8e2
Christian A. Wolf <mail@cawolf.de>
2020-12-27 12:31:14 +0100

    fix: update functional test

--- Tests/Functional/FunctionalTest.php
+++ Tests/Functional/FunctionalTest.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Auxmoney\OpentracingMonologBundle\Tests\Functional;
 
-use Auxmoney\OpentracingBundle\Tests\Functional\JaegerFunctionalTest;
+use Auxmoney\OpentracingBundle\Tests\Functional\JaegerConsoleFunctionalTest;
 use Symfony\Component\Process\Process;
 
-class FunctionalTest extends JaegerFunctionalTest
+class FunctionalTest extends JaegerConsoleFunctionalTest
 {
     public function testTraceContextInLog(): void
     {
@@ -21,19 +21,5 @@ class FunctionalTest extends JaegerFunctionalTest
 
         $logFileContent = file_get_contents('build/testproject/var/log/dev.log');
         self::assertContains($traceId, $logFileContent);
-    }
-
-    protected function setUpTestProject(string $projectSetup): void
-    {
-        $this->copyTestProjectFiles($projectSetup);
-
-        $this->composerDumpAutoload();
-        $this->consoleCacheClear();
-    }
-
-    protected function tearDown()
-    {
-        $this->gitResetTestProject();
-        $this->dockerStopJaeger();
     }
 }

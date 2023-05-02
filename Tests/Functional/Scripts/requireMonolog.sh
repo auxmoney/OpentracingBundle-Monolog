ccc1c14bc8ad53cd6c68e21db16579eba5a652bf
Christian A. Wolf <wolf@auxmoney.com>
2023-05-02 08:53:02 +0200

    fix: try reinstall

--- Tests/Functional/Scripts/requireMonolog.sh
+++ Tests/Functional/Scripts/requireMonolog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 shopt -s extglob
 
-composer require monolog/monolog:^${MONOLOG_VERSION} --update-with-dependencies
+composer reinstall monolog/monolog:^${MONOLOG_VERSION}
 composer dump-autoload
